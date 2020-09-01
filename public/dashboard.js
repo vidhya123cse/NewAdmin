7d7dc150097a100919f12e2d9012d7c8b65560c0
vidhya123cse <vidhyatr1998@gmail.com>
2020-09-01 21:40:48 +0530

    new

--- public/dashboard.js
+++ public/dashboard.js
@@ -7,3 +7,14 @@ function SignOut()
       
       window.location.href = "index.html";
 }
+
+
+//Submit form
+function formSubmit() {
+  // Get Values from the DOM
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var pos = document.getElementById("position").value;
+  console.log(name);
+}
