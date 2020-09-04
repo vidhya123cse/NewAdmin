49405768440e8c41c8b90afaf308536818f853cf
Surejmohan <soorajmohan121997@gmail.com>
2020-09-05 00:00:04 +0530

    lock

--- public/dashboard.js
+++ public/dashboard.js
@@ -102,3 +102,41 @@ function saveMessage(name, email, phone, pos){
   
 };
 
+
+function ExcelProcess()
+{
+  
+    if (files.length != 0) {
+      //Loops through all the selected files
+      for (let i = 0; i < files.length; i++) {
+        //create a storage reference
+        var storage = firebase.storage().ref(files[i].name);
+  
+        //upload file
+        var upload = storage.put(files[i]);
+  
+        //update progress bar
+        upload.on(
+          "state_changed",
+          function progress(snapshot) {
+            var percentage =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            document.getElementById("progress").value = percentage;
+          },
+  
+          function error() {
+            alert("error uploading file");
+          },
+  
+          function complete() {
+            document.getElementById(
+              "uploading"
+            ).innerHTML += `${files[i].name} upoaded <br />`;
+          }
+        );
+      }
+    } else {
+      alert("No file chosen");
+    }
+  
+}
